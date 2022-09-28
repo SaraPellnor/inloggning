827ab94a2d08448205f70ac5a943664a043c3545
SaraPellnor <114143422+SaraPellnor@users.noreply.github.com>
2022-09-28 12:38:13 +0100

    bara början

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 //console.log ("Hej");
-
-document.createElement("h1"); //här har vi skapat en h1:a, inte hämtat. Hämta gör vi med getElementById/Class
+/*Lagra elementet i en variabel, 
+i detta fall heading, så vi har det och kan jobba med det.*/  
+/*med "document.create.." har vi skapat en h1:a, 
+inte hämtat. (Hämta gör vi med getElementById/Class)*/
+const heading = document.createElement("h1");
