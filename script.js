a68b834ac8ea4781620c1898669730f77f5027b3
SaraPellnor <114143422+SaraPellnor@users.noreply.github.com>
2022-09-28 12:45:43 +0100

    tog bara bort lite grejjer i index eftersom den typ ska vara tom..

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ i detta fall heading, så vi har det och kan jobba med det.*/
 /*med "document.create.." har vi skapat en h1:a, 
 inte hämtat. (Hämta gör vi med getElementById/Class)*/
 const heading = document.createElement("h1");
-console.log (heading);
+console.log (heading);// consol.log skriver ut det nya elementet i konsollen via index.html.
+//Resultatet blir 2 taggar, <h1></h1>.
+
