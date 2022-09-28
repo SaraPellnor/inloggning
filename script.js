58c4e9fb6cac7c1aa6920de6ffd370d3ea95a8ae
SaraPellnor <114143422+SaraPellnor@users.noreply.github.com>
2022-09-28 12:44:20 +0100

    vi fortsätter..

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ i detta fall heading, så vi har det och kan jobba med det.*/
 /*med "document.create.." har vi skapat en h1:a, 
 inte hämtat. (Hämta gör vi med getElementById/Class)*/
 const heading = document.createElement("h1");
+console.log (heading);
